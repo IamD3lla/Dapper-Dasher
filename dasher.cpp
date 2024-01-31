d22194e61ee3bba538125f276c4b36a04a68b365
IamD3lla <jvictordgg@gmail.com>
2024-01-31 09:24:56 -0300

    Errors fixed!

--- dasher.cpp
+++ dasher.cpp
@@ -16,7 +16,7 @@ int main(){
     int windowDimensions[2]{windowDimensions[0] = 512, windowDimensions[1] = 380};
 
     //Initialize the window
-    InitWindow(windowDimensions[0], windowDimensions[1], "Dapper-Dasher");
+    InitWindow(windowDimensions[0], windowDimensions[1], "Dapper-Dasher!");
 
     int velocity{0};
 
@@ -24,18 +24,18 @@ int main(){
     //Nebula variables
     Texture2D nebula = LoadTexture("textures/12_nebula_spritesheet.png");
 
-    const int sizeOfNebulae{6};
+    const int sizeOfNebulae{10};
     //Array of custom data types
     animData nebulae[sizeOfNebulae]{};
 
     //for loop - Nebulas properties
     for(int i = 0; i < sizeOfNebulae; i++){
-        nebulae[i].rec.x = 0.0;
-        nebulae[i].rec.y = 0.0;
+        nebulae[i].rec.x = 0;
+        nebulae[i].rec.y = 0;
         nebulae[i].rec.width = nebula.width/8;
         nebulae[i].rec.height = nebula.height/8;
         nebulae[i].pos.x = windowDimensions[0] + i * 300;
-        nebulae[i].pos.y = windowDimensions[1] = nebula.height/8;
+        nebulae[i].pos.y = windowDimensions[1] - nebula.height/8;
         nebulae[i].frame = 0;
         nebulae[i].runningTime = 0.0;
         nebulae[i].updateTime = 1.0/16.0;
@@ -46,16 +46,12 @@ int main(){
 
     //Scarfy variables
     Texture2D scarfy = LoadTexture("textures/scarfy.png");
-    animData scarfyData {
-        scarfyData.rec.width = scarfy.width/6,
-        scarfyData.rec.height = scarfy.height,
-        scarfyData.rec.x = 0.0,
-        scarfyData.rec.y = 0.0,
-        scarfyData.pos.x = windowDimensions[0]/2 - scarfyData.rec.width/2,
-        scarfyData.pos.y = windowDimensions[1] - scarfyData.rec.height,
-        scarfyData.frame = 0,
-        scarfyData.updateTime = 1.0/12.0,
-        scarfyData.runningTime = 0.0
+    animData scarfyData{
+        {0, 0, scarfy.width/6, scarfy.height}, 
+        {windowDimensions[0]/2 - scarfyData.rec.width/2, windowDimensions[1] - scarfyData.rec.height},
+        0, 
+        1.0/12, 
+        0
     };
 
     //acceleration due to gravity (pixels/second)/second
